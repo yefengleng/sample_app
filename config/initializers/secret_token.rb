fbf20de0a488a4bb118de729753f2a3668ffc435
yefengleng <zg1236853196@aliyun.com>
2014-07-29 14:40:33 +0800

    Initial commit

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -12,8 +12,8 @@
 require 'securerandom'
 
 def secure_token
-  token_file = Rails.root.jion(' .secret')
-  if File.exist?(tolen_file)
+  token_file = Rails.root.join(' .secret')
+  if File.exist?(token_file)
     File.read(token_file).chomp
   else
     token = SecureRandom.hex(64)
